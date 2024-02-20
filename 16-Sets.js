d4868e1869b952151b1cdf6cd4becd45291ca1db
Umair Tauni <engrumayr@gmail.com>
2024-02-20 16:31:54 +0500

    modified

--- 16-Sets.js
+++ 16-Sets.js
@@ -23,3 +23,12 @@ console.log(ordersSet);
 for (const order of ordersSet) {
     console.log(order);
 }
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const staffUnique = new Set(staff);
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+console.log(new Set('umairtauni').size);
